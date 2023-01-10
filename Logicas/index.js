e12ea9465ff16572a701584286768dcad35b3aae
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-10 14:44:11 -0300

    exercicio-idade-do-cachorro

--- Logicas/index.js
+++ Logicas/index.js
@@ -32,11 +32,33 @@ const kelvin = 293;
 let c = 273;
 let contCelsius = kelvin - c;
 let celsius = contCelsius;
-console.log(celsius)
-
 let fahrenheit = celsius * (9/5) + 32;
-console.log(fahrenheit )
-
 let fahrenheitt = 68;
 let fahrenheitNumeroInteiro = Math.floor(fahrenheitt) 
 console.log(`A temperatura é ${fahrenheitNumeroInteiro} graus em Fahrenheit`)
+
+
+ /*
+#2 
+
+Idade de um cachorro
+
+Os cães amadurecem a um ritmo mais rápido do que os seres humanos.
+Costumamos dizer que a idade de um cachorro pode ser calculada em “anos de cachorro” para explicar seu crescimento em comparação com um humano da mesma idade. De certa forma, 
+poderíamos dizer que o tempo passa rapidamente para os cães – 8 anos na vida de um ser humano equivale a 45 anos na vida de um cão. Quantos anos você teria se fosse um cachorro?
+
+de “anos humanos” para “anos de cachorro”:
+
+Os primeiros dois anos de vida de um cão contam como 10,5 anos cada.
+Cada ano seguinte equivale a 4 anos de cão.
+Antes de começar a fazer as contas de cabeça, deixe um computador cuidar disso! 
+
+Com seu conhecimento de operadores matemáticos e variáveis,
+ use JavaScript para converter sua idade humana em anos de cachorro.
+*/
+
+let idadeHumana = 20;
+let doisPrimeirosAnos = 10.5 * 2;
+let restante =  18 * 4;
+const total = doisPrimeirosAnos + restante;
+console.log(`Idade do pablo convertida em anos de cachorro: ${total}`)
