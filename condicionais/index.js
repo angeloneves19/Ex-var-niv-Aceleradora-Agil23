d780f50f3e2c2016c722335c04fdd02a9be95179
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-13 14:58:33 -0300

    Exercicio2 if else

--- condicionais/index.js
+++ condicionais/index.js
@@ -1,4 +1,6 @@
+//1)
 //1.
+/*
 const soma = (num1 , num2 , num3) => {
   return num1 + num2 + num3
 }
@@ -24,7 +26,7 @@ const idadeAtual = (anoAtual, dataNasc) => {
 
 idadeAtual(2023, 2002)
 
-//4
+//4.
 const notaFinal = (nota1, nota2) => {
     let result = nota1 + nota2 / 2
     return result >= 5 ? console.log(`Você foi aprovado(a), com nota: ${result}`) 
@@ -32,4 +34,24 @@ const notaFinal = (nota1, nota2) => {
 }
 notaFinal(5, 5)
 
-//5
+//2)
+//DN dividir |
+const dividir = (num1, num2) => {
+    let result = num1 / num2 
+    if(num2 == 0){
+        console.log("Não pode ser feita a divisão, pois não existe divisão por zero")
+    }else{
+        return console.log(`A resposta é: ${result}`)
+    }
+}
+dividir(20, 2)
+*/
+//DN dividir ||
+const dividir2 = (numerador, denominador) => {
+   if(denominador === 0){
+        denominador = 3
+   }
+   let result = numerador / denominador;
+   console.log(result);
+}
+dividir2(15, 3)
