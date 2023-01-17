73c2efa606e4ef5f478640a9b94e47bd52840c0f
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-17 13:24:17 -0300

    atividade5-feita

--- Estrutura-de-decisao/index.js
+++ Estrutura-de-decisao/index.js
@@ -32,3 +32,13 @@ const positivoNegativoOuNulo = (num) => {
   }
 }
 positivoNegativoOuNulo(0)
+
+//5
+const adicaoESubtracao = (num1, num2) =>{
+let result = num1 + num2;
+return result > 20 ? console.log(`Dados do valor, somando 8: ${result + 8}`) 
+: console.log(`Dados do valor, subtraindo 5: ${result - 5}`)
+}
+adicaoESubtracao(10, 15);
+
+//6
