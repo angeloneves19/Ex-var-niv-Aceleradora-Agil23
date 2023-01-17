b8607fc20b9d38dc236071baecd554d56e2a8827
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-17 15:38:21 -0300

    Exercicio-7-feito

--- Estrutura-de-decisao/index.js
+++ Estrutura-de-decisao/index.js
@@ -51,3 +51,10 @@ console.log(calculoRaizQuadrada * calculoRaizQuadrada.toFixed(3))
 }
 } 
 raizQuadrada(0)
+
+//7
+const numeroMultiplicado = (num) => {
+var result = num % 3 === 0 ? console.log(`É multiplo de 3`) : console.log(`Não é multiplo de 3`);
+return result 
+}
+numeroMultiplicado(9);
