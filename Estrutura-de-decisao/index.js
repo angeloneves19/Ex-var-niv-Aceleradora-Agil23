92e72016a7560a1bd005ff19bc01bdf47ffe2bb8
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-18 10:02:19 -0300

    atividade-9-comcluida

--- Estrutura-de-decisao/index.js
+++ Estrutura-de-decisao/index.js
@@ -76,3 +76,10 @@ const divisivel = (a, b) => {
 };
 divisivel(18, 3)
 
+//9
+const MaiorOuMenor = (a, b) => {
+ a > b
+    ? console.log(`Valor do A:${a}, é maior que o B:${b}`)
+    : console.log(`Valor do B:${b}, é maior que o valor do A:${a}`);
+}
+MaiorOuMenor(12, 6)
