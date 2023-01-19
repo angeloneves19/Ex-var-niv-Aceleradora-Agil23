b06c492fc81d3b9200b9b97cf6e9877673770f83
Ashley Del Castillo <ashleydelcastillo@Ashleys-MacBook-Pro.local>
2023-01-19 10:25:52 -0300

    execicio-15/condicionais

--- Estrutura-de-decisao/index.js
+++ Estrutura-de-decisao/index.js
@@ -207,4 +207,11 @@ const divisivelPorNumero = (num) =>{
 }
 divisivelPorNumero(12)
 */
+
 //15
+const numeroVinteNoventa = (num) => {
+  return num > 20 && num < 90 
+  ? `este valor esta entre 20 e 90` 
+  : `20 e 90 não estão na faixa de valores`
+}
+console.log(numeroVinteNoventa(90))
