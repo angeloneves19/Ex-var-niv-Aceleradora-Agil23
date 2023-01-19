54042d7deefa320472f8a485f2c5ae8624b9ae81
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-19 13:44:26 -0300

    atividade-3-concluida

--- Estrutura-de-decisao/folha-3.js
+++ Estrutura-de-decisao/folha-3.js
@@ -9,6 +9,13 @@ const multiplicacaoDeTres = (numA, numB, numC) => {
   let multiplicacao = numA * numB * numC;
   return multiplicacao;
 };
-console.log(multiplicacaoDeTres(2, 2, 2));
+//console.log(multiplicacaoDeTres(2, 2, 2));
 
 //3
+const somaQuatro = ( num1, num2, num3, num4 ) =>{
+let soma = num1 + num2 + num3 + num4;
+return soma
+}
+//console.log(somaQuatro(5, 5, 5, 5));
+
+//4
