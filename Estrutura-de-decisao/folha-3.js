bc3db5e8e96c8472a42cac7a6e0a767e291fcfed
Ashley Del Castillo <ashleydelcastillo@Ashleys-MacBook-Pro.local>
2023-01-19 13:38:23 -0300

    atividade-2/folha-3

--- Estrutura-de-decisao/folha-3.js
+++ Estrutura-de-decisao/folha-3.js
@@ -1,8 +1,14 @@
 //1
-const subtracao = (num1, num2) =>{
-    return num1 + num2
-
-}
+const subtracao = (num1, num2) => {
+  return num1 + num2;
+};
 //console.log(subtracao(6, 6))
 
 //2
+const multiplicacaoDeTres = (numA, numB, numC) => {
+  let multiplicacao = numA * numB * numC;
+  return multiplicacao;
+};
+console.log(multiplicacaoDeTres(2, 2, 2));
+
+//3
