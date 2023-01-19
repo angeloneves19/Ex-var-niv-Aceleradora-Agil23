c1a62f79279fde7316f880f8f4d732ffc4b38748
Ashley Del Castillo <ashleydelcastillo@Ashleys-MacBook-Pro.local>
2023-01-19 15:23:04 -0300

    atividade 6

--- Estrutura-de-decisao/folha-3.js
+++ Estrutura-de-decisao/folha-3.js
@@ -1,4 +1,5 @@
 //1
+
 const subtracao = (num1, num2) => {
   return num1 + num2;
 };
@@ -23,11 +24,30 @@ const mediaAritmetica = ( num1, num2, num3, num4 ) =>{
     let somaAritmetica = num1 + num2 + num3 + num4 / 4;
     return somaAritmetica;
 }
+//5
 console.log(somaQuatro(6, 6, 6, 6));
 const KiloEmGrama = ( kilo ) =>{
 let grama = 1000
-return console.log(`Você está, com ${kilo * grama} gramas!`)
+return //console.log(`Você está, com ${kilo * grama} gramas!`)
 }
 //KiloEmGrama(50)
 
-//5
+ //6
+const loja = (valorEtiqueta, parcelas) =>{
+    let Avista = valorEtiqueta - ((valorEtiqueta * 10) / 100);
+    let duasParcelas = valorEtiqueta 
+    let quatroParcelas = valorEtiqueta + ((valorEtiqueta * 5) / 100)
+    if(parcelas === 0 || parcelas === 1 ){
+        console.log(`${Avista}`)
+    }
+    if(parcelas === 2){
+        console.log(`${duasParcelas}`)
+    }
+    if(parcelas === 4){
+        console.log(`${quatroParcelas}`)
+    }else{
+        return console.log(`Não temos como parcerlar este valor !!!`) 
+    }
+   
+    }
+loja(100, 5)
