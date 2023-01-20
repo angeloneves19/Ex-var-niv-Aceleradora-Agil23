fe42af06d7fa15298509edb902d26dc251357fd7
Ashley Del Castillo <ashleydelcastillo@Ashleys-MacBook-Pro.local>
2023-01-20 14:59:26 -0300

    andamento exercicio 10

---
+++ Estrutura-de-decisao/Folha-3/Exerc-10.js
@@ -0,0 +1,28 @@
+//10
+const diasDeVida = (nome, dia ,mes, ano) => {
+
+    //logica dias atuais
+    const diaAtual = 20
+    const MesAtual = 01
+    const anoAtual = 2023
+    
+    //cada 1 dia conta como 1 de vida
+    let diasContadosIdade = dia
+
+    //cada 1 ano conta como 365 dias de vida
+    let idadeAno = anoAtual - ano
+    let resultAno = idadeAno * 365
+
+    //cada 1 mes conta como 30 de vida
+   let diaPorMes = 
+   //soma da idade de claudio em dias 
+   return diasContadosIdade + resultAno + diaPorMes
+}
+console.log(diasDeVida('Claudio', 25 , 12, 2003))//resultado 6956 dias
+
+20/01/2023 
+15/02/2005
+
+idade = anoAtual - anoNascimento
+
+mes == mesAngelo 
