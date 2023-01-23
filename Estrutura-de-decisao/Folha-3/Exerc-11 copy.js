231efaa7114478752ac6a050e6202f2f9d9713f8
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-23 11:00:43 -0300

    atividade 11, arrumado o erro

---
+++ Estrutura-de-decisao/Folha-3/Exerc-11 copy.js
@@ -0,0 +1,5 @@
+//11
+const nomeSobrenome = (sobrenome, nome) => {
+    return `Sr(a) ${nome} ${sobrenome}, é uma honra contar com sua presença!`
+}
+console.log(nomeSobrenome('Néves', 'Ângelo'))
--- Estrutura-de-decisao/Folha-3/Exerc-12.js
+++ Estrutura-de-decisao/Folha-3/Exerc-12.js
@@ -1,8 +1,12 @@
 //12
+//1-Elevamos as medidas dos catetos ao quadrado;
+//2-Somamos os resultados;
+//3-Extraímos a raiz quadrada.
 const calculeAHipotenusa =  (b, c) => {
 let bAoQuarado = b * b
 let cAoQuarado = c * c
-
-return a = bAoQuarado + cAoQuarado
+let a = ( bAoQuarado + cAoQuarado) 
+let result = Math.sqrt(a);
+return `O valor da hipotenusa é: ${result}`
 }
 console.log(calculeAHipotenusa(12, 3))
