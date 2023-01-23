c90550e084557dc6bac2366c5808feb6b6f71986
Ashley Del Castillo <ashleydelcastillo@Ashleys-MacBook-Pro.local>
2023-01-23 15:44:32 -0300

    adicionando logica por commit

--- Estrutura-de-decisao/Folha-3/Exerc-15.js
+++ Estrutura-de-decisao/Folha-3/Exerc-15.js
@@ -1,4 +1,11 @@
 //15
+//criamos um funcao e passamos o numero por parametro
+//2)fizemos uma verificao pra ver se  o numero 
+//é positivo ou negativo
+//3)fizemos as operacoes dentro das variaveis
+//4)chamamos todas no return usando Template String 
+//chamando cada variavel
+
 const valoresNumPositivo = (num) => {
   if(num > 0){
     const aoQuadrado = num * num
