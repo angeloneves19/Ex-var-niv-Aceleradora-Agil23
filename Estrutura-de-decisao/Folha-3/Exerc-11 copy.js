cd1369b97428ef6d64797cd2f5fd1bdbb9ab7f74
Ashley Del Castillo <ashleydelcastillo@Ashleys-MacBook-Pro.local>
2023-01-23 11:13:16 -0300

    exercicio 13

--- Estrutura-de-decisao/Folha-3/Exerc-11 copy.js
+++
@@ -1,5 +0,0 @@
-//11
-const nomeSobrenome = (sobrenome, nome) => {
-    return `Sr(a) ${nome} ${sobrenome}, é uma honra contar com sua presença!`
-}
-console.log(nomeSobrenome('Néves', 'Ângelo'))
---
+++ Estrutura-de-decisao/Folha-3/Exerc-13.js
@@ -0,0 +1,6 @@
+const areaDoTriangulo = (base, altura) => {
+    //(base * altura)/2
+    let area = (base * altura)/2
+    return `A area do triagulo é igual a : ${area}cm`
+}
+console.log(areaDoTriangulo(50,100))
