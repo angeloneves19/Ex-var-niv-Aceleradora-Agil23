2c018c792ff2962166c627404063d57816890cdc
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-24 09:32:55 -0300

    Exercicio 16 concluido

---
+++ Estrutura-de-decisao/Folha-3/Exerc-16.js
@@ -0,0 +1,9 @@
+//16
+//1) Num1 recebe um valor, e o num2 recebe outro!
+//2)Dai elevamos o num1 pelo o num2.
+//3)E retornamos o resultado.
+const numeroElevado = (num1, num2) => {
+ return num1 ** num2;
+ 
+}
+console.log(numeroElevado(2, 4))
