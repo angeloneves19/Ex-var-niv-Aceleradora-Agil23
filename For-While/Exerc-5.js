0e6043944a58433f8c3f35a5dcf1fa181fb91469
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-25 16:00:09 -0300

    quase nofional

---
+++ For-While/Exerc-5.js
@@ -0,0 +1,14 @@
+//5
+const numerosPares = (m, n) => {
+  for (let i = m; i < n; i++) {
+    if ( i % 2 == 0 ) {
+      console.log(i)
+    }
+  }
+}
+numerosPares(0, 10)
+
+let contador = i++
+if( n == 0){
+  return contador
+}
