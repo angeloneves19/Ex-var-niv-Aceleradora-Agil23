e79fb0e5b31b73e382a66a77a052127468cbde09
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-25 10:53:34 -0300

    atividade-concluida-for

--- For-While/Folha-1/Exerc-1.js
+++ For-While/Folha-1/Exerc-1.js
@@ -3,6 +3,7 @@
 //2)Dai perguntamos se 0 é menor ou igual a 40.
 //3)Dai acresentamos +1, até chegar ao resultado desejado.
 //4)Dai retornamos o valor no console.log.
+//5)CONCLUIDO
 for( let i = 0; i <= 40; i++){
     console.log(i)
 }
