9d7f3130e6375e091ce1aa0b5a0c71047e29f521
Ashley Del Castillo <ashleydelcastillo@Ashleys-MacBook-Pro.local>
2023-01-26 14:48:57 -0300

    exercicio 7 totalmente finalizado

--- For-While/Exerc-7.js
+++ For-While/Exerc-7.js
@@ -3,9 +3,14 @@
 //2)Criamos o for, que foi basicamente dar a posição zero para o I.
 //dai dizemos enquanto o I for menor que o N, acresenta +1, 
 //até chegar no resultado desejado.
-const lendoNumeros = (m, n) =>{
-  for(let i = m; i < n; i++){
+
+//resolucao com for 
+//Por algum motivo quando queremos passar um valor maior que 1(i++) temos que adicionar o igual (i +=numero)
+const lendoNumeros = (valorInicial, incremento, valorFinal) =>{
+  for(let i = valorInicial; i < valorFinal; i +=incremento){
    console.log(i)
   }
 }
-lendoNumeros(0, 10);
+lendoNumeros(0, 4, 30);
+
+//resolucao com while
