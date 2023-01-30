f0118992e82415e958a05a8ea23c4461ac7b39d4
Ashley Del Castillo <ashleydelcastillo@Ashleys-MacBook-Pro.local>
2023-01-30 13:11:42 -0300

    exercicio 8 completo /exercico 9 incompleto

---
+++ For-While/exercicio9/index.js
@@ -0,0 +1,11 @@
+//9
+const repeticao = prompt('Quantos numeros gostaria de Colocar?')
+let numeroMaior = 0
+for (let i = 0; i < repeticao; i++) {
+  let valores = prompt('Digite um numero :')
+  if (valores > numeroMaior) {
+    numeroMaior = valores;
+     console.log(`Numero maior é: ${numeroMaior}`)   
+}if () { 
+}
+}
