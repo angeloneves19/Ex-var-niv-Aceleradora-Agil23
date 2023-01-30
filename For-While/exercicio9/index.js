5390e5f24cfd57feb4e7662934cc38edf74c4687
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-01-30 15:29:27 -0300

    exercicio-9-em-desenvolvimento

--- For-While/exercicio9/index.js
+++ For-While/exercicio9/index.js
@@ -1,11 +1,29 @@
 //9
-const repeticao = prompt('Quantos numeros gostaria de Colocar?')
-let numeroMaior = 0
+const repeticao = prompt("Quantos numeros gostaria de Colocar?");
+let numeroMaior = 0;
+//let numeroMenor = 0;
 for (let i = 0; i < repeticao; i++) {
-  let valores = prompt('Digite um numero :')
+  let valores = prompt("Digite um numero :");
+  //let resultadoArr = math.max(valores.split(""));
   if (valores > numeroMaior) {
     numeroMaior = valores;
-     console.log(`Numero maior é: ${numeroMaior}`)   
-}if () { 
+    console.log(`Numero maior é: ${numeroMaior}`)
+  }else if(valores > numeroMaior) {
+    numeroMaior = valores;
+    console.log(`Numero maior é: ${numeroMaior}`);
+  }else if(valores > numeroMaior){
+    numeroMaior = valores;
+    console.log(`Numero maior é: ${numeroMaior}`);
+  }
+    
 }
-}
+/*
+const myvar = (numero) => {
+  let cont = 0;
+  while (numero > cont) {
+    cont++;
+    console.log(cont);
+  }
+};
+myvar(10)
+*/
