fcf1cb9015a4f99aee28e86859fb1c8cf98d34d0
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-01 15:42:40 -0300

    ativididade-10-concluida

--- For-While/Exerc-10.js
+++ For-While/Exerc-10.js
@@ -1,18 +1,18 @@
-const somaDePares = (começo, fim) => {
-    let comecoNumInteiro = parseInt(começo)//verificao numeros Inteiros
-    let fimInteiro = parseInt(fim)
+const somaDePares = (inicio, fim) => {
+    let inicioNumeroInteiro = parseInt(inicio)//verificao numeros Inteiros
+    let fimNumeroInteiro = parseInt(fim)
     let soma = 0
     if (começo > 0 && fim > 0 ) {//verificacao pra saber se tanto o comeco quanto o fim sao positivos
-        for (let i = comecoNumInteiro; i <= fimInteiro; i++) {
+        for (let i = inicioNumeroInteiro; i <= fimNumeroInteiro; i++) {
             if (i % 2 === 0) {
                 soma = soma + i
-                console.log(i)
+                console.log(`Numeros pares, entre eles: ${i}`)
             }
         }    
     }else{
         console.log('Escolha um numero Posivo')
     }
-    console.logç(soma)
+    console.log(`Resultado dos numeros, somados: ${soma}`)
 }
 
 somaDePares(2, 20)//determinie os valores
