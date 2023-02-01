929833d472ce6640e930de82080688158d560ce2
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-01 15:25:21 -0300

    exercio 10 completo

--- For-While/Exerc-10.js
+++ For-While/Exerc-10.js
@@ -1,16 +1,20 @@
-const myfunc = (começo, fim) => {
+const somaDePares = (começo, fim) => {
     let comecoNumInteiro = parseInt(começo)//verificao numeros Inteiros
     let fimInteiro = parseInt(fim)
+    let soma = 0
     if (começo > 0 && fim > 0 ) {//verificacao pra saber se tanto o comeco quanto o fim sao positivos
         for (let i = comecoNumInteiro; i <= fimInteiro; i++) {
             if (i % 2 === 0) {
+                soma = soma + i
                 console.log(i)
             }
         }    
     }else{
         console.log('Escolha um numero Posivo')
     }
+    console.logç(soma)
 }
-myfunc(2, 20.5)//determinie os valores
+
+somaDePares(2, 20)//determinie os valores
 
 //
