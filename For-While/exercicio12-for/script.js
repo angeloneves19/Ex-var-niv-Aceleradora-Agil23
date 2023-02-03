0f024757282566b70beb40714f11c66a4144a72d
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-03 11:58:47 -0300

    12 em execucao

---
+++ For-While/exercicio12-for/script.js
@@ -0,0 +1,10 @@
+const somaValoresDigitados = () => {
+    let condição = true
+    let contadorNumero = 0
+    while(condição){
+        let leNumero = prompt('digite um numero (obs: -1 para a contagem)')
+        if(leNumero > 0){
+            
+        }
+    }
+}
