9e524fba9b1c940e97ab86691a2a6364dac674cc
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-06 10:52:46 -0300

    em execucao

--- For-While/exercicio12-for/script.js
+++ For-While/exercicio12-for/script.js
@@ -1,12 +1,18 @@
 const somaValoresDigitados = () => {
-   // let contadorNumero = 0 
-
-
-    while(true){
-        let leNumero = parseInt(prompt('digite um numero (obs: -1 para a contagem'))
-        if (leNumero == -1) {
-         return false
-        }
+  let contadorNumero = 0
+  while (true) {
+    let leNumero = parseInt(
+      prompt("digite um numero (obs: -1 para a contagem")
+    );
+    if (leNumero == -1) {
+      return false
     }
+    if (leNumero > 0) {
+     let result  = contadorNumero + leNumero
+     console.log(result)
+    }
+  } 
+ 
 }
-console.log(somaValoresDigitados())
+console.log(somaValoresDigitados());
+
