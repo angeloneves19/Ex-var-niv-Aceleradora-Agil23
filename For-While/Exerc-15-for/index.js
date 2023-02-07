d4e6f588249fb77a2303dd0260157e663929772e
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-07 11:55:30 -0300

    Exercicio-15-concluido

--- For-While/Exerc-15-for/index.js
+++ For-While/Exerc-15-for/index.js
@@ -1,24 +1,43 @@
 const mediaParOuImpar = () => {
-  let condicao = true
+  let condicao = true;
   let contadorDeNumerosPares = 0
   let contadorDeNumerosImpares = 0
+  let somaDosNumerosPares = 0
+  let mediaNumerosPares = 0
+  let contadorGeral = 0
+  let somaGeral = 0
+  let mediaGeral = 0
+
   while (condicao) {
-    let leNumero = prompt(
-      "Digite um numero: (obs: Digite 0, para parar a contagem!"
+    let leNumero = parseInt(
+      prompt("Digite um numero: (obs: Digite 0, para parar a contagem!")
     )
     if (leNumero > 0) {
-      leNumero % 2 === 0
-        ? contadorDeNumerosPares++
-        : contadorDeNumerosImpares++
+        `
+      ${(somaGeral = somaGeral + leNumero)}
+      ${contadorGeral++}
+      ${(mediaGeral = somaGeral / contadorGeral)}
+      `
+      if (leNumero % 2 === 0) {
+        `
+         ${(somaDosNumerosPares = somaDosNumerosPares + leNumero)}
+         ${contadorDeNumerosPares++}
+         ${(mediaNumerosPares = somaDosNumerosPares / contadorDeNumerosPares)}
+        `
+      } else {
+        contadorDeNumerosImpares++
+      }
+    }
+    if (leNumero === 0) {
+      condicao = false
     }
   }
-  if (leNumero == 0) {
-    condicao = false
-  }
-
-return `
+  return `
 Existem ${contadorDeNumerosPares} numeros Pares, nessa funcão
 Existem ${contadorDeNumerosImpares} numeros Impares, nessa funcão
+A media dos Valores Pares é: ${mediaNumerosPares}!
+A media geral é: ${mediaGeral}
 `
 }
-console.log(mediaParOuImpar());
+
+console.log(mediaParOuImpar())
