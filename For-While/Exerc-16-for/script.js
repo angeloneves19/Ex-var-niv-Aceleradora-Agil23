91da925882667a306e9656fca00a94b8f30502ad
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-08 11:47:27 -0300

    exercicio 16 completo

--- For-While/Exerc-16-for/script.js
+++ For-While/Exerc-16-for/script.js
@@ -1,20 +1,22 @@
 const alunoComMaiorMedia = () => {
-  condicao = true;
-  let maiorMedia = 0;
+  condicao = true
+  let maiorMedia = 0
+  let guardarNomes = '' 
   while (condicao) {
     let nomeAluno = prompt("Digite o nome do aluno");
     let medias = parseInt(prompt(`Qual a media do ${nomeAluno} ?`));
     let parando = prompt("Deseja Parar Digite Pare(se sim responda sim)");
     medias >= 6
       ? alert(`Parabens ${nomeAluno} voce passou com media media ${medias}`)
-      : alert(`O aluno ${nomeAluno} reprovou com media ${medias}`);
+      : alert(`O aluno ${nomeAluno} reprovou com media ${medias}`)
     if (medias > maiorMedia) {
-      maiorMedia = medias;
+      maiorMedia = medias
+      guardarNomes = nomeAluno
     }
     if (parando === "sim") {
-      condicao = false;
-      return `o aluno ${nomeAluno} teve a maior media ${maiorMedia}`;
+      condicao = false; 
+      return document.write(`o aluno ${guardarNomes} teve a maior media ${maiorMedia}`)
     }
   }
-};
+}
 console.log(alunoComMaiorMedia());
