7f4b1098f99eb63371e05d1e2ad8f4d041132b22
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-08 11:14:20 -0300

    em execucao

--- For-While/Exerc-16-for/script.js
+++ For-While/Exerc-16-for/script.js
@@ -1,13 +1,20 @@
 const alunoComMaiorMedia = () => {
-  while (true) {
-    let nomeAluno = prompt("Digite o nome do aluno")
-    let media = prompt(`Qual a media do ${nomeAluno} ?`)
-    media >= 6
-     ? alert(`Parabens ${nomeAluno} voce passou com media media ${media}`)
-     : alert(`O aluno ${nomeAluno} reprovou com media ${media}`)
-    if (nomeAluno === "pare" || media === 11 ) {
-       break
+  condicao = true;
+  let maiorMedia = 0;
+  while (condicao) {
+    let nomeAluno = prompt("Digite o nome do aluno");
+    let medias = parseInt(prompt(`Qual a media do ${nomeAluno} ?`));
+    let parando = prompt("Deseja Parar Digite Pare(se sim responda sim)");
+    medias >= 6
+      ? alert(`Parabens ${nomeAluno} voce passou com media media ${medias}`)
+      : alert(`O aluno ${nomeAluno} reprovou com media ${medias}`);
+    if (medias > maiorMedia) {
+      maiorMedia = medias;
+    }
+    if (parando === "sim") {
+      condicao = false;
+      return `o aluno ${nomeAluno} teve a maior media ${maiorMedia}`;
     }
   }
-}
+};
 console.log(alunoComMaiorMedia());
