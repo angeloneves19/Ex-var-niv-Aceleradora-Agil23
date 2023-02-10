dbb3f4d8d96e27861f83a6344dfbf6016e3ff3cc
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-10 11:56:37 -0300

    exercicio 18 completo

---
+++ For-While/Folha-1/Exerc-18.js
@@ -0,0 +1,13 @@
+//18
+const polegadasPorCentimetros = (polegadas, centimetros) => {
+    for(i = polegadas; i <= 20; i++){
+        let conversao = centimetros * i
+        console.log(`${i} pol = ${conversao} cm`)
+
+} 
+}
+polegadasPorCentimetros(1, 2.54)
+
+
+// 1 p = 2.54
+// 2 p = 5.8
