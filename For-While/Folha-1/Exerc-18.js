6e4bd37480dc9836c58c70d648203287de611ad0
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-10 13:27:40 -0300

    exercicio 18 completo e comentado

--- For-While/Folha-1/Exerc-18.js
+++ For-While/Folha-1/Exerc-18.js
@@ -1,11 +1,15 @@
 //18
+// criammos  a funcao passando  os dados  por parametros
 const polegadasPorCentimetros = (polegadas, centimetros) => {
+    // criamos a repeticao de 20 para a tabela 
     for(i = polegadas; i <= 20; i++){
+        // e criamos a conversao passando no console e estruturando com Template String.
         let conversao = centimetros * i
         console.log(`${i} pol = ${conversao} cm`)
 
 } 
 }
+// passamos os dados e chamamos a funcao
 polegadasPorCentimetros(1, 2.54)
 
 
