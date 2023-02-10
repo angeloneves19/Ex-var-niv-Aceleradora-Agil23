4789026d38e0945e3775b66d4c7d387b7dcdad6d
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-10 14:17:59 -0300

    trablho-19-completo-refatorado-e-comentado

--- For-While/Folha-1/Exerc-19.js
+++ For-While/Folha-1/Exerc-19.js
@@ -1,8 +1,8 @@
 //19
-//Criamos um variavel global, para imprimir tudo no console.lop.
+//Criamos um variavel global, para imprimir tudo no console.log.
 //Depois criamos um estrutura do For, para fazer a repetição, de um a dez, pois é uma tabuada!
 //Em seguida, criamos uma variavel chamada de multiplicação, para fazer a multiplicação dos numeros!
-//Por fim, estilizamos eles no console.log, utilizando, templete String!
+//Por fim, estilizamos eles no console.log, utilizando templete String!
 const imprimirTabuada = (numeroInicial, numeroDaTabuada) => {
   for (let i = numeroInicial; i <= 10; i++) {
     let multiplicacao = i * numeroDaTabuada
