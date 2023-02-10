7a6b35d03e7e6311ca5f512430b86d2eb9f5eac4
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-10 14:52:49 -0300

    18 refatorado

--- For-While/Folha-1/Exerc-19.js
+++ For-While/Folha-1/Exerc-19.js
@@ -4,7 +4,6 @@
 //Em seguida, criamos uma variavel chamada de multiplicação, para fazer a multiplicação dos numeros!
 //Por fim, estilizamos eles no console.log, utilizando templete String!
 //completo
-const num = 0 
 const imprimirTabuada = (numeroInicial, numeroDaTabuada) => {
   for (let i = numeroInicial; i <= 10; i++) {
     let multiplicacao = i * numeroDaTabuada
@@ -14,4 +13,4 @@ const imprimirTabuada = (numeroInicial, numeroDaTabuada) => {
   }
 }
 
-imprimirTabuada(1, 5)
+imprimirTabuada(1, 99)
