90aff4a4b0179cee47157bdac7d4924e6d71dee5
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-13 13:42:10 -0300

    20 em desenvolvimento

--- For-While/Exerc-20-for/script.js
+++ For-While/Exerc-20-for/script.js
@@ -2,12 +2,16 @@ const mostrarRaizesQuadradasInteiras = () => {
     let condicao = true
     let guardarRaizInteira = 0
   while (condicao) {
-    let guardaNumero = parseInt(prompt("Digite um numero (obs: 0 para a contagem)"))
+    let guardaNumero = Number(prompt("Digite um numero (obs: 0 para a contagem)"))
     if (guardaNumero === 0) {
       condicao = false
     }else{ 
-        console.log(Math.sqrt(guardaNumero))
+      let root = Math.sqrt(guardaNumero)
+      if (root == parseInt(root)) {
+         guardarRaizInteira = root + ' quadrado perfeito'
+         console.log(guardarRaizInteira)
+      }
     }
   }
-};
+}
 mostrarRaizesQuadradasInteiras()
