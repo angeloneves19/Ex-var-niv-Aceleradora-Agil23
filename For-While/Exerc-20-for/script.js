5fb2cdd15f64089b082226de28eed1e9c3cce9b7
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-13 15:43:34 -0300

    20 completa sem comentarios

--- For-While/Exerc-20-for/script.js
+++ For-While/Exerc-20-for/script.js
@@ -1,17 +1,18 @@
+
 const mostrarRaizesQuadradasInteiras = () => {
     let condicao = true
-    let guardarRaizInteira = 0
+    let contador = 0
   while (condicao) {
     let guardaNumero = Number(prompt("Digite um numero (obs: 0 para a contagem)"))
     if (guardaNumero === 0) {
       condicao = false
     }else{ 
       let root = Math.sqrt(guardaNumero)
-      if (root == parseInt(root)) {
-         guardarRaizInteira = root + ' quadrado perfeito'
-         console.log(guardarRaizInteira)
+      if (root === parseInt(root)) {
+         contador++
       }
     }
   }
+  console.log(`Existem ${contador} numeros interos digitados`)
 }
 mostrarRaizesQuadradasInteiras()
