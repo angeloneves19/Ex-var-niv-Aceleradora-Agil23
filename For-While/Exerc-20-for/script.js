601a37c5070d3a24c8051a694919f31a7906ee32
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-13 15:58:43 -0300

    atividade-20-comentada

--- For-While/Exerc-20-for/script.js
+++ For-While/Exerc-20-for/script.js
@@ -1,4 +1,10 @@
-
+//Criamos uma variavel global, para ler tudo!
+//Depois criamos um let condição verdadeira para inicar o loop while!
+//Em seguida criamos um let guardarRaizInteira para armazenar os resultados dos numeros quadrados perfeitos!
+//Depois iniciamos o while utilizando um prompt para saber os numeros desejados pelos usuarios.
+//Dai fizemos uma verificação para fazer a parada do loop while, quando o numero fosse zero iria parar!
+//Se nao, fizemos uma verificaco da raiz, e tornamos numeros interiros para termos quadrados exatos!
+//E por fim passamos um contador, para saber quantos quadrados exatos, haviam sidos capturados pela variavel guardar Numero!
 const mostrarRaizesQuadradasInteiras = () => {
     let condicao = true
     let contador = 0
