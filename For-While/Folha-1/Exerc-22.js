37807f67efce7c836057fc652a4b6815c1597c0a
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-14 15:57:21 -0300

    Atividade-22-completa-so-falta-comentar

--- For-While/Folha-1/Exerc-22.js
+++ For-While/Folha-1/Exerc-22.js
@@ -1,4 +1,11 @@
 //22
+//Primeiro criamos uma variavel global, para imprimir o resultado na tela!
+//Depois criamos um laço  For, para descobrir o   Quociente de acordo, 
+//com os numeros digitados pelo usuario!
+//Então dentro do For, criamos um variavel chamada de i,
+//que recebia o valor do dividendo digitada pelo usuario!
+//Depois dizemos enquanto i(dividendio)
+
 const subtracaoQuocienteInteiro = (dividendo, divisivel) => {
   for (let i = dividendo ; i > 0; i-=divisivel) {
     console.log(i)
