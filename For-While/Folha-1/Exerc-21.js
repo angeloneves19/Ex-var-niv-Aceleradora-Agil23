659419e0d31c5e93881a9e8f6ed82851c3179e0f
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-14 11:20:20 -0300

    atividade-21-concluida-e-comentada

--- For-While/Folha-1/Exerc-21.js
+++ For-While/Folha-1/Exerc-21.js
@@ -1,7 +1,21 @@
 //21
+//Primeiro criamos uma variavel global, para passar tudo certinho!
+//Depois criamos uma variavel para armazenar os numeros divididos!
+//Em seguida criamos um laço de repeetição For.
+//Depois criamos uma verificação Se o resto da divisão fosse 0 ele contaria!
+//Dai em seguida criamos uma verificação Se o numero que restou da verificação acima fosse 2 o numero seria primo!
+//Se não, ele não seria primo!
 const numerosPrimos = (num) => {
-  return num % 1 === 1  && num * num === 0
-    ? `num nao é numero primo`
-    : `este num é primo`
+  let divisores = 0
+  for (let i = 1; i <= num; i++) {
+    if (num % i == 0) {
+      divisores++
+    }
+  }
+  if (divisores == 2) {
+    console.log(`É primo`)
+  } else {
+    console.log(`Não é primo`)
+  }
 }
-console.log(numerosPrimos(4))
+numerosPrimos(4)
