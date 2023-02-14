23cbba55362104e73421ac4980f0daf143b634f9
PablinhoHunter <hunterofhunter331@gmail.com>
2023-02-14 13:20:46 -0300

    22 concluida

---
+++ For-While/Folha-1/Exerc-22.js
@@ -0,0 +1,7 @@
+//22
+const subtracaoQuocienteInteiro = (dividendo, divisivel) => {
+  for (let i = dividendo ; i > 0; i-=divisivel) {
+    console.log(i)
+  }
+}
+subtracaoQuocienteInteiro(100, 12)
