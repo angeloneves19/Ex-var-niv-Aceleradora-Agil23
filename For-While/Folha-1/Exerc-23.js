52a11b5cf81dc48002548cf1564a9e8727e9cf78
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-15 14:12:27 -0300

    atividade-23-concluido

--- For-While/Folha-1/Exerc-23.js
+++ For-While/Folha-1/Exerc-23.js
@@ -3,9 +3,10 @@ let contadorSubtracoes = 0
 const tirandoARaiz = (num) => {
   for (let i = 1; i <= num; i++) {
     if(i % 2 !== 0){
-       contadorSubtracoes++
+     num = num - i
+      contadorSubtracoes++
     }
   }
 console.log(contadorSubtracoes)
 }
-tirandoARaiz(16)
+tirandoARaiz(25)
