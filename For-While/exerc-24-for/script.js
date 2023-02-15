3a5ba4f95f5b5390fdf5926b280a1e9a1f39f7a4
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-15 15:52:13 -0300

    ativdade-24-concluida-e-comentada

--- For-While/exerc-24-for/script.js
+++ For-While/exerc-24-for/script.js
@@ -1,4 +1,11 @@
 // 24
+//Primeiro criamos uma função global, para imprimir tudo!
+//Depois criamos um contador para armazenar a resposta final!
+//Dai criamos uma estrutura de For, com o numero de parada igual a 5, 
+//pois no enunciado so poderia haver 5 numeros digitados pelos usuarios!
+//Dai criamos uma verificação para passar somente numeros abaixo de zero(somente numeros negativos).
+//E por fim chamamos o contador para nos dizer somente os numeros negativos que foram digitados!
+//Dai so passamos uma mensagem no cosole.log chamando o contador com as respostas!
 const contarNumerosNegativos = () => { 
     contador = 0
    for(i = 0; i < 5; i++){
