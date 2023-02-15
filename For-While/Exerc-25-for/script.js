b56842f67d8af0e99ebec9a3aed3b137f7f14fc7
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-15 15:59:09 -0300

    Ativividade-25-em-deselvovmento

---
+++ For-While/Exerc-25-for/script.js
@@ -0,0 +1,5 @@
+const empresa = () =>{
+    let codigo = prompt("Digite seu codigo da empresa: ")
+    let sexo = prompt("Digite seu sexo: ")
+    let horasTrabalhadas = prompt("Digite suas horas trabalhadas na empresa: ")
+}
