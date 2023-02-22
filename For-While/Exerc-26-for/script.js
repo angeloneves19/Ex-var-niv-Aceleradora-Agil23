70bda16f260c42f59444ba1da08bd9c1f0eb061e
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-02-22 11:35:54 -0300

    exercicio 26 em execucao

---
+++ For-While/Exerc-26-for/script.js
@@ -0,0 +1,58 @@
+const pesquisaEntreHabitantes = () => {
+  let Condicao = true
+  let armazena = 0
+  let contador = 0
+  let contadorPaes = 0
+  let maiorSalario = 0
+  let contadorDePessoasComSalarioDeCem = 0
+  while (Condicao) {
+    let trabalhadoresNaCasa = Number(
+      prompt("Quantas Pessoas trabalham na sua casa?")
+    );
+    for (i = 1; i <= trabalhadoresNaCasa; i++) {
+      let renda = Number(prompt(`Qual a renda da Pessoa ${i} ?`));
+      armazena += renda;
+      contador++;
+      if (renda > maiorSalario) {
+        maiorSalario = renda
+      }
+      if (renda <= 100) {
+        contadorDePessoasComSalarioDeCem++
+        let percentualAteCem = renda
+        console.log(
+          "percentual  de pessoas com salario cem = " +
+            percentualAteCem * (contadorDePessoasComSalarioDeCem / 100)
+
+        )
+      }
+    }
+    console.log("media de salarios " + (armazena / contador).toFixed(1));
+    console.log("maior salario " + maiorSalario)
+
+    if (trabalhadoresNaCasa === 0) {
+      Condicao = false
+    } else {
+      let quantidadesPaes = confirm("voce tem Filhos ? (sim ou não)");
+      if (quantidadesPaes) {
+        contadorPaes++
+        console.log(contadorPaes)
+        let quantidadeFilhos = parseInt(prompt("Quantos filhos você tem ?"));
+      } else {
+        console.log("")
+      }
+      console.log(quantidadeFilhos)
+    }
+  }
+};
+pesquisaEntreHabitantes()
+
+// angelo
+// 2
+// pessoa 2
+// 4
+// pessoa 3
+//1
+
+// 2 + 4 + 1 / 3
+
+// console.log(armazena / contador)
