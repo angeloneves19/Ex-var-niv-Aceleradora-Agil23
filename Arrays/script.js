2772adbe4ae466a45e87b221ddde90b50008f76b
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-02-23 13:15:17 -0300

    1/array concluido

---
+++ Arrays/script.js
@@ -0,0 +1,7 @@
+//1. Crie um algoritmo que leia um vetor de 10 posições
+// e depois apresente este vetor em ordem inversa na tela.
+
+const arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(arry.reverse())
+
+
