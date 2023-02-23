d07c2abf2924041d654949699d298275a1fc09bc
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-02-23 15:56:57 -0300

    3/array concluido

--- Arrays/Exerc-2.js
+++ Arrays/Exerc-2.js
@@ -1,6 +1,15 @@
 // 2. Crie um algoritmo que leia um vetor de 30 números
 //inteiros e gere um segundo vetor cujas posições pares são o
 //dobro do vetor original e as ímpares o triplo.
+
+//-Criamos dois arrys vazios globais para armazenar valores dos
+//contadores i e as verificações abaixo
+//-Criamos uma Funcao Para armazenar a nossa logica e passamos o 
+//numero de parada do contador por parametros.
+//-Criamos o contador para Colocarmos no arry
+//- Fizemos uma verificao if para termos os numeros pares e multiplicar por dois(dobro),
+//fazendo o mesmo com os impares porem usando o triplo(x3) e adicionando-os no Arry2
+//passamos o valor da  parrada e o resultado por console.log
 const arry = []
 const arry2 = []
 const parOuImpar = (numero) => {
---
+++ Arrays/Exerc.3-arry/Exerc-3.js
@@ -0,0 +1,19 @@
+/* Faça um programa que preencha um vetor com oito números inteiros,
+ calcule e mostre dois vetores resultantes. O primeiro deve conter os números positivos;
+ o segundo deve conter os números negativos. Cada vetor resultante vai ter, no máximo,
+  oito posições, que poderão não ser completamente utilizadas.*/
+
+  let vetor = []
+  let vetor2 = []
+  let vetor3 = []
+
+  const myFunc = () => {
+    for(i= 1; i <= 8 ;i++){
+        let leNumero = parseInt(prompt(`Digite 1 numero por favor`))
+        vetor.push(leNumero)
+        // if (condition) {
+        // }
+  }
+}
+  myFunc()
+  console.log(vetor) 
