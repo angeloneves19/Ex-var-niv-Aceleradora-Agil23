3839a3b320877f02d6cec1774680babd51b7ae0f
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-02-24 11:03:07 -0300

    exercicio-3/em andamento

--- Arrays/Exerc.3-arry/Exerc-3.js
+++ Arrays/Exerc.3-arry/Exerc-3.js
@@ -1,19 +1,23 @@
 /* Faça um programa que preencha um vetor com oito números inteiros,
- calcule e mostre dois vetores resultantes. O primeiro deve conter os números positivos;
- o segundo deve conter os números negativos. Cada vetor resultante vai ter, no máximo,
-  oito posições, que poderão não ser completamente utilizadas.*/
+  calcule e mostre dois vetores resultantes. O primeiro deve conter os números positivos;
+  o segundo deve conter os números negativos. Cada vetor resultante vai ter, no máximo,
+    oito posições, que poderão não ser completamente utilizadas.*/
 
-  let vetor = []
-  let vetor2 = []
-  let vetor3 = []
+let vetor = [];
+let numerosPositivos = [];
+let numerosNegativos = [];
 
-  const myFunc = () => {
-    for(i= 1; i <= 8 ;i++){
-        let leNumero = parseInt(prompt(`Digite 1 numero por favor`))
-        vetor.push(leNumero)
-        // if (condition) {
-        // }
-  }
-}
-  myFunc()
-  console.log(vetor) 
+
+// const myFunc = () => {
+//   for (i = 1; i <= 8; i++) {
+//     let leNumero = parseInt(prompt(`Digite 1 numero por favor`));
+//     vetor.push(leNumero);
+//   }
+//   if (leNumero < 0) {
+//     numerosPositivos.push(leNumero)
+//   } else {
+//     numerosNegarivos.push(leNumero)
+//   }
+// }
+// myFunc();
+// console.log(numerosPositivos);
