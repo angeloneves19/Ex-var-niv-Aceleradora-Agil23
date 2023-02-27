568d8924a4bf3ebd91548142fa72d8111d108a89
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-02-27 11:57:33 -0300

    em execucao

--- Arrays/Exerc-6.js
+++ Arrays/Exerc-6.js
@@ -4,19 +4,22 @@ calcule e mostre quantas notas estão em um intervalo de valores
 que vai de 10% abaixo da média até 10% acima dela.
 */
 
-const vetorOriginal = [2, 3, 4, 5, 1, 6, 9, 10, 7, 8]
-const vetorRepetido = []
-let soma = 0
-for (let i = 0; i < 5; i++) {
-  vetorRepetido.push(...vetorOriginal);
-  if (media - mediaDezPorcento && media + mediaDezPorcento) {
-    console.log(media)
-  }
-  for (let i = 0; i < vetorRepetido.length; i++) {
-    soma += vetorRepetido[i]
+const leitorDeMediasVetor = () => {
+  const vetorOriginal = [2, 3, 4, 5, 1, 6, 9, 10, 7, 8]
+  const vetorRepetido = []
+  let soma = 0
+  for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < vetorRepetido.length; i++) {
+      soma += vetorRepetido[i]
+    }
+    let media = soma / vetorRepetido.length
+    let mediaDezPorcento = (media * 10) / 100
+    vetorRepetido.push(...vetorOriginal)
+    if (media < media - mediaDezPorcento && media > media + mediaDezPorcento) {
+      console.log(media)
+    }
+    console.log(soma)
+    console.log(mediaDezPorcento)
   }
 }
-console.log(soma)
-let media = soma / vetorRepetido.length
-let mediaDezPorcento = (media * 10) / 100
-console.log(mediaDezPorcento)
+leitorDeMediasVetor()
