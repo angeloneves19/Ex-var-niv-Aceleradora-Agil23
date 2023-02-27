806b87ecddf01ec5928d680f3bd67b41f3b35fd1
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-02-27 11:18:44 -0300

    atividade-6-em-desenvolvimento

--- Arrays/Exerc-6.js
+++ Arrays/Exerc-6.js
@@ -4,15 +4,19 @@ calcule e mostre quantas notas estão em um intervalo de valores
 que vai de 10% abaixo da média até 10% acima dela.
 */
 
-const vetorOriginal = [2, 3, 4, 5, 5, 6, 6, 10, 7, 8]
+const vetorOriginal = [2, 3, 4, 5, 1, 6, 9, 10, 7, 8]
 const vetorRepetido = []
 let soma = 0
 for (let i = 0; i < 5; i++) {
-  vetorRepetido.push(...vetorOriginal)
+  vetorRepetido.push(...vetorOriginal);
+  if (media - mediaDezPorcento && media + mediaDezPorcento) {
+    console.log(media)
+  }
   for (let i = 0; i < vetorRepetido.length; i++) {
     soma += vetorRepetido[i]
   }
 }
 console.log(soma)
 let media = soma / vetorRepetido.length
-console.log(media)
+let mediaDezPorcento = (media * 10) / 100
+console.log(mediaDezPorcento)
--- For-While/Folha-1/Exerc-24.js
+++ For-While/Folha-1/Exerc-24.js
@@ -1,1 +1,19 @@
 // 24
+// Função para randomizar array
+function shuffleArray(arr) {
+    // Loop em todos os elementos
+for (let i = arr.length - 1; i > 0; i--) {
+        // Escolhendo elemento aleatório
+    const j = Math.floor(Math.random() * (i + 1));
+    // Reposicionando elemento
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+// Retornando array com aleatoriedade
+return arr;
+}
+var arrA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(shuffleArray(arrA));
+console.log(shuffleArray(arrA)); 
+console.log(shuffleArray(arrA)); 
+console.log(shuffleArray(arrA));
+console.log(shuffleArray(arrA));
