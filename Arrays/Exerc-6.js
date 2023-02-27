12b0464d7d970b8f666b8c3008c2f52914edc00f
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-02-27 10:47:45 -0300

    6 em execucao

--- Arrays/Exerc-6.js
+++ Arrays/Exerc-6.js
@@ -4,16 +4,15 @@ calcule e mostre quantas notas estão em um intervalo de valores
 que vai de 10% abaixo da média até 10% acima dela.
 */
 
-// let vetor = [2, 3, 4, 5, 5, 6, 6, 10, 7, 8]
-
-// for (let i = vetor.length; i < 50; i++) {
-//    vetor.push(i)
-//    console.log(vetor)
-//   }
-
-const vetorOriginal = [2, 3, 4, 5, 5, 6, 6, 10, 7, 8];
-const vetorRepetido = [];
+const vetorOriginal = [2, 3, 4, 5, 5, 6, 6, 10, 7, 8]
+const vetorRepetido = []
+let soma = 0
 for (let i = 0; i < 5; i++) {
-  vetorRepetido.push(vetorOriginal);
+  vetorRepetido.push(...vetorOriginal)
+  for (let i = 0; i < vetorRepetido.length; i++) {
+    soma += vetorRepetido[i]
+  }
 }
-console.log(vetorRepetido); 
+console.log(soma)
+let media = soma / vetorRepetido.length
+console.log(media)
