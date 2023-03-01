cb115d1f318e1ef6318c95f2a3359ad187a27268
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-03-01 15:53:26 -0300

    atividade-8-em-deselvovimento

---
+++ Arrays/Exerc-8.js
@@ -0,0 +1,66 @@
+/*
+8. Faça um algoritmo que leia o nome, o custo e o preço
+de venda de 50 produtos. Ao final deverá informar a
+quantidade de produtos que:
+a. Tem lucro menor que 10%;
+b. Tem lucro entre 10% e 30%;
+c. Tem lucro maior que 30%
+*/
+const produtos = []
+const loja = (nome, custo, preço) =>{
+    return produtos.push({
+     nome: nome,
+     custo: custo,
+     preço: preço
+    })
+}
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
+loja("Nike Air Max", 500, 1000)
