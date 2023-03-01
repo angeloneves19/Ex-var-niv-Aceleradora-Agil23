d6513d032c6729c9c3e1bc35cc93e915b798c62d
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-03-01 14:02:13 -0300

    atvdade-7-em-deselvolvimento

--- Arrays/Exerc-7.js
+++ Arrays/Exerc-7.js
@@ -21,53 +21,58 @@ preocupe com empates).
 let armazena = 0
 const trasformandoEmVetor = []
 const postProduct = (nome, valor) => {
-    return trasformandoEmVetor.push(
-    {
-        id: trasformandoEmVetor.length + 1,
-        nome: nome,
-        valor: valor,
-        ganhoDoVendedor: valor * 5 / 100
-    })
+  return trasformandoEmVetor.push({
+    id: trasformandoEmVetor.length + 1,
+    nome: nome,
+    valor: valor,
+    ganhoDoVendedor: (valor * 5) / 100
+  })
 }
 postProduct("Lampada Magica", 359)
 postProduct("Sino", 219.05)
 postProduct("Escultura em ouro", 1867)
-postProduct("Piramide", 80.90)
-postProduct("Nicho Budista", 76.60)
-postProduct("Fonte de água", 206.30)
+postProduct("Piramide", 80.9)
+postProduct("Nicho Budista", 76.6)
+postProduct("Fonte de água", 206.3)
 postProduct("Escultura Girafa", 163.86)
-postProduct("Escultura Galinha", 39.90)
-postProduct("Bonecas Camponesas", 113.90)
+postProduct("Escultura Galinha", 39.9)
+postProduct("Bonecas Camponesas", 113.9)
 postProduct("Ganesha Bronze", 1140)
 
 const carrinho = []
 const getProduct = (nameProduct, amount) => {
-  const Product = trasformandoEmVetor.find(products => products.nome === nameProduct)
-  const ganhoPorPorcentagemProduto = (Product.valor * amount) * 5 / 100 
+  const Product = trasformandoEmVetor.find(
+    (products) => products.nome === nameProduct
+  )
+  const ganhoPorPorcentagemProduto = (Product.valor * amount * 5) / 100
   const salárioFinal = ganhoPorPorcentagemProduto + 400
   let totalProdutos = Product.valor * amount
-
   carrinho.push({
     id: Product.id,
     nome: nameProduct,
+    valor: Product.valor * amount,
     quantidade: amount
-  
   })
-   carrinho.map(numero => {
-    if(numero.quantidade > armazena){
-      armazena = numero.quantidade
-      console.log(` O produto ${numero.nome} do codigo N°${numero.id} foram um dos mais vendidos, com a quantidade de ${numero.quantidade} peças`)
-    }
-   })
-  
 
-  // console.log(`-------------via-cliente----------------------`)
-  // console.log(`Total De compras: R$ ${totalProdutos.toFixed(2)}`)
-  // console.log(`Comissão do vendedor: R$ ${ganhoPorPorcentagemProduto.toFixed(2)}`)
-  // console.log(`-------------via-funcionario------------------`)
-  // console.log(`Salario com adicionais do cliente ${salárioFinal.toFixed(2)}`)
+  //console.log(`-------------via-cliente----------------------`)
+  //console.log(`Total De compras: R$ ${totalProdutos.toFixed(2)}`)
+  //console.log(
+ //   `Comissão do vendedor: R$ ${ganhoPorPorcentagemProduto.toFixed(2)}`
+ // )
+ /// console.log(`-------------via-funcionario------------------`)
+ // console.log(`Salario com adicionais do cliente ${salárioFinal.toFixed(2)}`)
 }
-getProduct('Bonecas Camponesas', 3)
-getProduct('Sino', 4)
-getProduct('Escultura em ouro', 9)
+getProduct("Bonecas Camponesas", 4);
+getProduct('Sino', 9)
+getProduct('Escultura em ouro', 3)
 getProduct('Fonte de água', 2)
+
+
+//criar carrinho . find
+//sort() ordena numeros
+//pop() pega o ultimo
+//depois criar uma variavel global, para englobar tudo!
+//depois usa um map para mapear e puxxar tudo!
+
+const a = carrinho.map(products => products.quantidade).sort().pop()
+console.log(carrinho.find(products => products))
