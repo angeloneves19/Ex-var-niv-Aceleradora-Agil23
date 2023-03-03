3c71ed887e8bd5b3c028b06e1c8cdfe7c7a26da5
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-03-03 11:58:56 -0300

    ativdade-teste-em-desenvolvmento

---
+++ Arrays/Exerc-Estudos.js
@@ -0,0 +1,33 @@
+// * criar uma variavel de nome person
+// * criar uma funcao com nome addPerson
+// * fazer com que essa funcao adcione um obj em um array com as seguintes informacoes
+// - id
+// - nome
+// - data de nascimento
+// - status
+// - peso
+// - altura
+
+const person = []
+const addPerson = (nome, nascimento, peso, altura) => {
+  const pushPerson = person.push({
+    id: person.length + 1,
+    nome: nome,
+    nascimento: nascimento,
+    status: "inativo",
+    peso: peso,
+    altura: altura
+  })
+}
+
+addPerson("Angelo", "14/02/2005", 135, 180)
+addPerson("Pablo", "14/05/2002", 66, 178)
+addPerson("Juliao", "19/05/1990", 80, 183)
+//console.log(person[2].altura)
+
+const tornandoAtivo = (nome) =>{
+  const a =  person.find(pessoa => pessoa.nome === nome )
+    return a.status = "ativo"
+}
+console.log(tornandoAtivo("Pablo"))
+console.log(person)
