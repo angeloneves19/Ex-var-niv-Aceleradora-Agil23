f894b0fa8d434d597e3602d973ed8928e70abbb8
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-03-06 09:54:40 -0300

    atividade-8-nova-completa-pablo-ira-comentar

--- Arrays/Exerc-8/script.js
+++ Arrays/Exerc-8/script.js
@@ -11,18 +11,20 @@ let vetorItem = []
 let vetorValorCompra = []
 let vetorLucro = []
 
+for (let i = 0; i < 5; i++) {
+  let nomeProduto = prompt("Qual é o nome do item ?")
+  let custoDeCompra = Number(prompt("Quanto voce Pagou por Este Item?"))
+  let lucroDeVenda = Number(prompt("Por Quanto voce vai vender este item?"))
+  vetorItem.push(nomeProduto)
+  vetorValorCompra.push(custoDeCompra)
+  vetorLucro.push(lucroDeVenda)
 
-for (let i = 0; i < 2; i++) {
-  let nomeProduto = prompt('Qual é o nome do item ?')
-  let custoDeCompra = Number(prompt('Quanto voce Pagou por Este Item?'))
-  let lucroDeVenda = Number(prompt('Por Quanto voce vai vender este item?'))
-   vetorItem.push(nomeProduto)
-   vetorValorCompra.push(custoDeCompra)
-   vetorLucro.push(lucroDeVenda)
-   
-   let porcentagemDeDez = ((custoDeCompra * 10 ) / 100 )  + custoDeCompra
-   let resultadoLucroDez = lucroDeVenda - porcentagemDeDez
-   console.log(resultadoLucroDez)
+  let porcentagemDeDez = (custoDeCompra * 10) / 100 + custoDeCompra
+  let porcentagemDeTrinta = (custoDeCompra * 30) / 100 + custoDeCompra
+  let resultadoLucroDez = lucroDeVenda - porcentagemDeDez
+  let resultadoLucroTrinta = lucroDeVenda - porcentagemDeTrinta
+  console.log(`ucro 10%; ${resultadoLucroDez}`)
+  console.log(`Lucro 30%; ${resultadoLucroTrinta}`)
 }
 console.log(vetorItem)
 console.log(vetorValorCompra)
