a60d23fb487ed77df4d4349d7119cf16c1bfcdb0
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-03-06 15:57:12 -0300

    11 em andamento

---
+++ Arrays/Exerc-11.js
@@ -0,0 +1,22 @@
+/*11.
+ Faça um programa para corrigir provas de múltipla
+escolha. Cada prova tem oito questões e cada questão vale um
+ponto. O primeiro conjunto de dados a ser lido é o gabarito da
+prova. Os outros dados são os números dos alunos e as
+respostas que deram às questões. Existem dez alunos
+matriculados. Calcule e mostre:
+a. O número e a nota de cada aluno;
+b. A percentagem de aprovação, sabendo-se que a nota
+mínima é 6.
+ */
+let questoes = []
+let gabarito = []
+const folha = gabarito.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+const test = gabarito.push('d', 'b', 'c','c','d','a')
+console.log(gabarito)
+
+ 
+
+
+
+// questao1 = 'd' ,questao2 = 'b', qustao3 = 'c', questao4 = 'c', questao5 = 'd', questao6 = 'a'
