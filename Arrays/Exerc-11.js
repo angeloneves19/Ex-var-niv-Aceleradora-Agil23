5f6cc81a85d0c8ffe5b916cdb15254646e775e67
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-03-07 15:55:19 -0300

    atividade-11-em-deselvovimento

--- Arrays/Exerc-11.js
+++ Arrays/Exerc-11.js
@@ -8,7 +8,68 @@
   a. O número e a nota de cada aluno;
   b. A percentagem de aprovação, sabendo-se que a nota
   mínima é 6.
+    let nomes = ["Gabarito" ,"Carlos Eduardo" , "Danilo", "Fabio", "Gabriel", "Igor", "Julio", "Matheus", "Pablo", "Renatiinho"]
   */
+const vetorAlunos = []
+const addAlunos = (nome, gabarito) =>{
+    vetorAlunos.push({
+        nome: nome,
+        gabarito: gabarito,
+    })
+}
+
+addAlunos("Gabarito" ,['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b'])
+addAlunos("Carlos Eduardo" ,['c', 'd', 'c', 'c', 'a', 'a', 'd', 'b'])
+addAlunos("Danilo"  ,['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b'])
+addAlunos("Fabio" ,['b', 'd', 'c', 'a', 'b', 'a', 'c', 'd'])
+addAlunos("Gabriel" ,['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b'])
+addAlunos("Igor" ,['a', 'b', 'd', 'a', 'c', 'a', 'a', 'b'])
+addAlunos("Julio" ,['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b'])
+addAlunos("Matheus" ,['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b'])
+addAlunos("Pablo" , ['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b'])
+addAlunos("Renatiinho" ,['d', 'b', 'a', 'c', 'd', 'a', 'a', 'b'])
+console.log(vetorAlunos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
   let gabarito = []
   let contador = 0
   gabarito.push('d', 'b', 'c', 'c', 'd', 'a', 'a', 'b')
@@ -32,4 +93,4 @@
   //   }
   //   console.log(contador)
   // }
-  
+  */
