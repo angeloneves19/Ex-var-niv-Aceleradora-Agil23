583acbe63c3ce24ffa6406f0b34cc0ff29996d29
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-03-07 11:58:37 -0300

    em execucao

--- Arrays/Exerc-10.js
+++ Arrays/Exerc-10.js
@@ -7,7 +7,7 @@ atenda a esta condição.
 */
 //-Criamos um vetor com 10 numeros aleatorios.
 //-passamos por console um mapeamento  do vetor passando um template string num if ternario sabermos os valores maiores que 50
-//e chamarmos o vetor e sua posicao por o metodo indexOf() .
+//e chamarmos o vetor e sua posicao por o metodo indexOf().
 const vetor = [10, 130, 200, 100, 38, 29, 34, 8, 29, 500]
 console.log(
   vetor.map((num) =>
--- Arrays/Exerc-11.js
+++ Arrays/Exerc-11.js
@@ -1,22 +1,35 @@
-/*11.
- Faça um programa para corrigir provas de múltipla
-escolha. Cada prova tem oito questões e cada questão vale um
-ponto. O primeiro conjunto de dados a ser lido é o gabarito da
-prova. Os outros dados são os números dos alunos e as
-respostas que deram às questões. Existem dez alunos
-matriculados. Calcule e mostre:
-a. O número e a nota de cada aluno;
-b. A percentagem de aprovação, sabendo-se que a nota
-mínima é 6.
- */
-let questoes = []
-let gabarito = []
-const folha = gabarito.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-const test = gabarito.push('d', 'b', 'c','c','d','a')
-console.log(gabarito)
-
- 
-
-
-
-// questao1 = 'd' ,questao2 = 'b', qustao3 = 'c', questao4 = 'c', questao5 = 'd', questao6 = 'a'
+  /*11.
+  Faça um programa para corrigir provas de múltipla
+  escolha. Cada prova tem oito questões e cada questão vale um
+  ponto. O primeiro conjunto de dados a ser lido é o gabarito da
+  prova. Os outros dados são os números dos alunos e as
+  respostas que deram às questões. Existem dez alunos
+  matriculados. Calcule e mostre:
+  a. O número e a nota de cada aluno;
+  b. A percentagem de aprovação, sabendo-se que a nota
+  mínima é 6.
+  */
+  let gabarito = []
+  let contador = 0
+  gabarito.push('d', 'b', 'c', 'c', 'd', 'a', 'a', 'b')
+  //console.log(gabarito)
+  let nomes = ["Gabarito" ,"Carlos Eduardo" , "Danilo", "Fabio", "Gabriel", "Igor", "Julio", "Matheus", "Pablo", "Renatiinho"]
+  nomes[0] = ['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b']//8
+  nomes[1] = ['c', 'd', 'c', 'c', 'a', 'a', 'd', 'b']//4
+  nomes[2] = ['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b']//8
+  nomes[3] = ['b', 'd', 'c', 'a', 'b', 'a', 'c', 'd']//3
+  nomes[4] = ['a', 'b', 'd', 'a', 'c', 'a', 'a', 'b']//4
+  nomes[5] = ['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b']//8
+  nomes[6] = ['c', 'b', 'c', 'a', 'd', 'a', 'a', 'a']//5
+  nomes[7] = ['d', 'b', 'c', 'c', 'd', 'a', 'a', 'b']//8
+  nomes[8] = ['b', 'b', 'c', 'a', 'd', 'a', 'a', 'a']//5
+  nomes[9] = ['d', 'b', 'a', 'c', 'd', 'a', 'a', 'b']//7
+  //  console.log(nomes)
+  for(let i = 0 ; i < nomes.length; i++){
+    if(gabarito.toString() === nomes[i].toString()){
+      contador++
+      // console.log(nomes[i])
+    }
+    console.log(contador)
+  }
+  
