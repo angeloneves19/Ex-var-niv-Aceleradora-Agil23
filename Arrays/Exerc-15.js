397f1188d58818339092abeebca45aa917487adf
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-03-14 15:18:25 -0300

    nao finalizado/Ajuda

--- Arrays/Exerc-15.js
+++ Arrays/Exerc-15.js
@@ -1,44 +1,50 @@
-/*15. Faça um programa que preencha um vetor com os modelos de cinco carros. 
-Carregue outro vetor com o consumo destes carros, isto é, quantos quilômetros 
-cada um deles faz com um litro de combustível,calcule e mostre:
-a. O modelo de carro mais econômico.
-b. Quantos litros de combustível cada um dos carros cadastrados consomem 
-para percorrer uma distância de 1.000 quilômetros.*/
-const carros = []
+  /*15. Faça um programa que preencha um vetor com os modelos de cinco carros. 
+  Carregue outro vetor com o consumo destes carros, isto é, quantos quilômetros 
+  cada um deles faz com um litro de combustível,calcule e mostre:
+  a. O modelo de carro mais econômico.
+  b. Quantos litros de combustível cada um dos carros cadastrados consomem 
+  para percorrer uma distância de 1.000 quilômetros.*/
+const carros = [] 
 const consumoPorLitro = []
-const teste = []
 
 const concenssionaria = (carro, kmPorLitro) => {
   carros.push(carro);
   consumoPorLitro.push(kmPorLitro)
 }
 concenssionaria("logan 1.0", 14)
-concenssionaria("fiat uno 1.0", 16.6)
-concenssionaria("kwid", 15.7)
-concenssionaria("hb20 1.6", 12.5)
-concenssionaria("gol g7", 13.3)
+concenssionaria("fiat uno 1.0", 16)
+concenssionaria("kwid", 15)
+concenssionaria("hb20 1.6", 12)
+concenssionaria("gol g7", 13)
+  let kmMenor = consumoPorLitro[0]
+for(let i = 0; i < consumoPorLitro.length ; i++){
+if(consumoPorLitro[i] < kmMenor){
+  kmMenor = i
+}
+}
+console.log(carros[kmMenor])
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // console.log(carros)
 // console.log(consumoPorLitro)
-
-
-
 
   // teste.push({
   //   id: teste.length + 1,
   //   nome: carros,
   //   kmPorLitro: consumoPorLitro,
   // })
-
-  for(let i = 0; i < carros.length ; i++){
-  //const result = consumoPorLitro.filter(num => num > num)
-
-  return console.log(consumoPorLitro.map(num => num > num ? "achei" : "nao"))
-  }
-
-
-
-
 
 
 
