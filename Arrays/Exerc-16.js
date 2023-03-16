b6d648f073d9a4d6379f00a4461c851e9032a87d
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-03-16 10:03:01 -0300

    16 finalizado

--- Arrays/Exerc-16.js
+++ Arrays/Exerc-16.js
@@ -9,41 +9,20 @@ iii. Repita estas operações envolvendo agora apenas os 19
 elementos restantes (trocando o de menor valor com a
 segunda posição), depois os 17, os 16 e assim por
 diante, até restar um único elemento, o maior deles.
-
-
-
-//exercicio 16 -vetores
-let vetor = [4, 3, 10, 5, 19, 20, 17, 16, 1, 7, 9, 8, 0, 30, 29, 44, 31, 40, 50];
-let indice = 0;
-let vetorCrescente = [];
-let menorNumero;
-while (vetor.length > 0) {
-  menorNumero = vetor[0];
-  for (i = 0; i < vetor.length; i++) {
-    if (vetor[i] <= menorNumero) {
-      menorNumero = vetor[i];
-      indice = i;
-    }
-  }
-  vetorCrescente.push(menorNumero);
-  vetor.splice(indice, 1);
-}
-//console.log(TESTANDO  ${vetor});
-console.log(vetorCrescente);
-console.log(`O elemento do vetor que apresenta o menor valor é ${vetorCrescente[0]}`)
 */
 
 function bubbleSort() {
   const numeros = [0, 20, 17, 19, 33, 11, 100, 18, 10, 7, 8, 3, 2, 1, 14, 16, 15, 9]
+  let result
   for (let i = 0; i < numeros.length; i++) {
     for (let j = 0; j < numeros.length - (i-1); j++) {
-      if (numeros[i] > numeros[i + 1]) {
-        let result = numeros[i]
-        numeros[i] = numeros[i + 1]
-        numeros[i + 1] = result
+      if (numeros[j] > numeros[j + 1]) {
+        result = numeros[j + 1]
+        numeros[j + 1] = numeros[j]
+        numeros[j] = result
       }
-     }
+    }
   } 
+  return numeros
 }
-
-bubbleSort()
+console.log(bubbleSort())
