6fcb371b13e3a58d5a2cbaa88c08f67d54393caf
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-03-16 15:25:50 -0300

    atividade-pokemon-em-deselvovimento

--- ExercicioJS/objetos.js
+++ ExercicioJS/objetos.js
@@ -14,25 +14,50 @@ Places:
     - pallet
     - cerulean
 */
-let pokemonn = "pikachu, 3, eletric, (thunderShock - QUICK attack - thuNder), STATIC, (VERIDIAN, CERULEAN, PALLET)"
+
+let pokemonn =
+  "pikachu, 3, eletric, (thunderShock - QUICK attack - thuNder), STATIC, (VERIDIAN, CERULEAN, PALLET)"
 let transforme = pokemonn.split(",")
-// console.log(transforme  
-const pkm = [] 
+// console.log(transforme
+const pikachu = transforme[0].replace("p", "P")
+const atacks = transforme[3].split("-")
+const place = transforme[5].split(",")
+
+const pkm = []
 const objeto = () => {
-    pkm.push({
-        nome: transforme[0],
-        lv: transforme[1], 
-        type:transforme[2],
-        ability: transforme[4],
-        atacks: transforme[3],
-        place: transforme[6]
-    })
-}
+  pkm.push({
+    nome: pikachu,
+    level: transforme[1],
+    type: transforme[2],
+    ability: transforme[4],
+    atacks: transforme[3].split("-")[],
+    place1: transforme[5],
+  });
+console.log(`
+
+Name: ${pkm[0].nome}
+Lv: ${pkm[0].level}  - Type: ${pkm[0].type.toUpperCase()}
+Ability: ${pkm[0].ability.toLowerCase()}
+Attacks:
+    - Thundershock,
+    - Quick attack
+    - Thunder
+
+Places:
+    - veridian
+    - pallet
+    - cerulean
+    
+    `);
+};
+
+//objeto();
+
 objeto()
-console.log(pkm)
+console.log(pkm);
 
 //
-// 
-// 
-// 
-// 
+//
+//
+//
+//
